ab6da42b6454026f10c572f8c0a375a7487f36ce
Harini <harini140307@gmail.com>
2026-06-22 19:58:27 +0530

    Update changes

--- _core/settings.py
+++ _core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*','.onrender.com']
 CSRF_TRUSTED_ORIGINS = [ 'https://*' ]
 
 # Application definition
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+STATICFILES_DIRS = [ BASE_DIR / 'static' ]\
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media' 
